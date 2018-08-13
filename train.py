724ef58fd830edf1131b9874534abb383b472ad8
Andrew Doyle <andrew.doyle@mcgill.ca>
2018-08-13 12:08:35 -0400

    saving custom loss

--- train.py
+++ train.py
@@ -39,17 +39,17 @@ def unet(n_tissues):
     # bn1 = BatchNormalization()(conv1)
     pool1 = MaxPooling3D(pool_size=pool_size)(conv1)
 
-    conv2 = Conv3D(32, big_conv_size, activation='relu', padding='same', use_bias=False)(pool1)
+    conv2 = Conv3D(16, big_conv_size, activation='relu', padding='same', use_bias=False)(pool1)
     # conv2 = Conv3D(8, big_conv_size, activation='relu', padding='same', use_bias=False)(conv2)
     # bn2 = BatchNormalization()(conv2)
     pool2 = MaxPooling3D(pool_size=pool_size)(conv2)
 
-    conv3 = Conv3D(64, big_conv_size, activation='relu', padding='same', use_bias=False)(pool2)
+    conv3 = Conv3D(32, big_conv_size, activation='relu', padding='same', use_bias=False)(pool2)
     # conv3 = Conv3D(8, big_conv_size, activation='relu', padding='same', use_bias=False)(conv3)
     # bn3 = BatchNormalization()(conv3)
     pool3 = MaxPooling3D(pool_size=pool_size)(conv3)
 
-    conv4 = Conv3D(128, big_conv_size, activation='relu', padding='same', use_bias=False)(pool3)
+    conv4 = Conv3D(32, big_conv_size, activation='relu', padding='same', use_bias=False)(pool3)
     # conv4 = Conv3D(8, big_conv_size, activation='relu', padding='same', use_bias=False)(conv4)
     # bn4 = BatchNormalization()(conv4)
     pool4 = MaxPooling3D(pool_size=pool_size)(conv4)
@@ -58,11 +58,11 @@ def unet(n_tissues):
     # bn5 = BatchNormalization()(conv5)
     # pool5 = MaxPooling3D(pool_size=pool_size)(bn5)
 
-    conv6 = Conv3D(128, small_conv_size, activation='relu', padding='same', use_bias=False)(pool4)
+    conv6 = Conv3D(64, small_conv_size, activation='relu', padding='same', use_bias=False)(pool4)
     drop6 = Dropout(0.5)(conv6)
-    conv7 = Conv3D(128, small_conv_size, activation='relu', padding='same', use_bias=False)(pool4)
+    conv7 = Conv3D(64, small_conv_size, activation='relu', padding='same', use_bias=False)(pool4)
     drop7 = Dropout(0.5)(conv7)
-    conv8 = Conv3D(128, mini_conv_size, activation='relu', padding='same', use_bias=False)(pool4)
+    conv8 = Conv3D(64, mini_conv_size, activation='relu', padding='same', use_bias=False)(pool4)
     drop8 = Dropout(0.5)(conv8)
     nadir = add([drop6, drop7, drop8])
     # bn8 = BatchNormalization()(nadir)
@@ -74,19 +74,19 @@ def unet(n_tissues):
 
     skip10 = concatenate([pool4, nadir])
     up10 = UpSampling3D(size=pool_size)(skip10)
-    conv10 = Conv3D(128, big_conv_size, activation='relu', padding='same', use_bias=False)(up10)
+    conv10 = Conv3D(32, big_conv_size, activation='relu', padding='same', use_bias=False)(up10)
     # conv10 = Conv3D(8, big_conv_size, activation='relu', padding='same', use_bias=False)(conv10)
     # bn10 = BatchNormalization()(conv10)
 
     skip11 = concatenate([pool3, conv10])
     up11 = UpSampling3D(size=pool_size)(skip11)
-    conv11 = Conv3D(64, big_conv_size, activation='relu', padding='same', use_bias=False)(up11)
+    conv11 = Conv3D(32, big_conv_size, activation='relu', padding='same', use_bias=False)(up11)
     # conv11 = Conv3D(8, big_conv_size, activation='relu', padding='same', use_bias=False)(conv11)
     # bn11 = BatchNormalization()(conv11)
 
     skip12 = concatenate([pool2, conv11])
     up12 = UpSampling3D(size=pool_size)(skip12)
-    conv12 = Conv3D(32, big_conv_size, activation='relu', padding='same', use_bias=False)(up12)
+    conv12 = Conv3D(16, big_conv_size, activation='relu', padding='same', use_bias=False)(up12)
     # conv12 = Conv3D(8, big_conv_size, activation='relu', padding='same', use_bias=False)(conv12)
     # bn12 = BatchNormalization()(conv12)
 
@@ -98,10 +98,10 @@ def unet(n_tissues):
 
     conv14 = Conv3D(64, small_conv_size, activation='relu', padding='same', use_bias=False)(conv13)
     # conv14 = Conv3D(16, small_conv_size, activation='relu', padding='same', use_bias=False)(conv14)
-    drop14 = Dropout(0.5)(conv14)
+    drop14 = Dropout(0.6)(conv14)
     # bn14 = BatchNormalization()(drop14)
     conv15 = Conv3D(64, small_conv_size, activation='relu', padding='same', use_bias=False)(drop14)
-    drop15 = Dropout(0.5)(conv15)
+    drop15 = Dropout(0.6)(conv15)
     # bn15 = BatchNormalization()(drop15)
     conv16 = Conv3D(16, mini_conv_size, activation='relu', padding='same', use_bias=False)(drop15)
 
@@ -130,7 +130,7 @@ def batch(data_dir, subj_ids):
 
             # normalize img_array by modalities
             for m in range(nmods):
-                img_array[0,:,:,:,m] = (img_array[0,:,:,:,m] - np.min(img_array[0,:,:,:,m])) / (np.max(img_array[0,:,:,:,m]) + 0.000001)
+                img_array[0, ..., m] = (img_array[0, ... , m] - np.min(img_array[0, ..., m])) / (np.max(img_array[0, ..., m]) + 0.000001)
 
             segmentation_img = nib.load(data_dir + subj_id + '/segm.nii.gz').get_data()
             # will be 9 or 10
@@ -153,18 +153,18 @@ def dice_coef(y_true, y_pred, smooth=10**(-5)):
     :return: Scalar DICE coefficient.
     """
     # exclude the background and infarction class from DICE calculation
-    exclude = [9,10]
+    exclude = [9, 10]
     # y_true is (1,240,240,48,11)
     # shape = y_true.get_shape()
     # num_classes = 11#shape[-1]
     labels = range(num_classes) # [i for i in range(num_classes) if i not in exclude]
-    class_weight=[1.33277636e-01,1.73610281e+00,2.86846186e+01,2.42313665e+00,1.89505171e+03,
-            2.12526369e+00,2.36024193e+01,6.79663033e+00,4.66165505e+01]
+    class_weight=[1.33277636e-01, 1.73610281e+00, 2.86846186e+01, 2.42313665e+00, 1.89505171e+03,
+            2.12526369e+00, 2.36024193e+01, 6.79663033e+00, 4.66165505e+01]
     class_weight = [w/sum(class_weight) for w in class_weight]
     score = 0
-    for w,l in zip(class_weight,labels):
-        intersection = K.sum( y_true[..., l] * y_pred[..., l] )
-        score += w * ( 2.0 * intersection + smooth ) / ( K.sum(y_true[..., l]) + K.sum(y_pred[..., l]) + smooth )
+    for w, l in zip(class_weight,labels):
+        intersection = K.sum(y_true[..., l] * y_pred[..., l])
+        score += w * (2.0 * intersection + smooth) / (K.sum(y_true[..., l]) + K.sum(y_pred[..., l]) + smooth)
     return score / len(labels)
 
 
@@ -215,6 +215,6 @@ if __name__ == '__main__':
 
     model.fit_generator(batch(data_dir, subjects[0:-1]), len(subjects) - 1, epochs=n_epochs, validation_data=batch(data_dir, [subjects[-1]]), validation_steps=1, max_queue_size=2, workers=1, callbacks=[checkpoint])
 
-    best_model = load_model(data_dir + 'best_segmentation_model.hdf5')
+    best_model = load_model(data_dir + 'best_segmentation_model.hdf5',custom_objects={'dice_coef': dice_coef, 'dice_coef_loss': dice_coef_loss})
 
     best_model.save(data_dir + 'neuroMTL_segmentation.hdf5')
